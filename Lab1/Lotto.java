ebf4606a3638aaf2cdec821c0be0d94610dfde3a
arturkupiec <artur_kupiec@o2.pl>
2025-10-06 17:56:17 +0200

    lotto niedokonczone

---
+++ Lab1/Lotto.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Lotto{
+    public static void main(String[] args){
+        for (int i = 0; i < 6; i++){
+            Random losowa = new Random();
+            int losowa1 = losowa.nextInt(49);
+            System.out.println(losowa1);            
+        }
+
+    }
+}
