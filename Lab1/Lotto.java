2949ad04cfe950b82fc0261ddf8bbccd9331dd44
arturkupiec <artur_kupiec@o2.pl>
2025-10-06 18:00:31 +0200

    prasdasdas

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 
 public class Lotto{
     public static void main(String[] args){
+
         for (int i = 0; i < 6; i++){
             Random losowa = new Random();
-            int losowa1 = losowa.nextInt(49);
+            int losowa1 = losowaasd.nextInt(49);
             System.out.println(losowa1);            
         }
 
