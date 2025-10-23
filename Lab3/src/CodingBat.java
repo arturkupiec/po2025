2cad29263981c1edbd2bb63beb1cc2bf7a111005
artur <artur_kupiec@o2.pl>
2025-10-23 20:42:04 +0200

    plik CodingBatTest.java uzupelniony

--- Lab3/src/CodingBat.java
+++ Lab3/src/CodingBat.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 public class CodingBat {
 
     public int close10(int a, int b) {
@@ -10,9 +12,49 @@ public class CodingBat {
         }
     }
 
-    public boolean in3050(int a, int b) {
-        return( a<= 40 && a>=30 && b<=40 && b>=30 || a>=40 && a<= 50 && b<=50 && b>=40);
+    public int factorial(int n) {
+        int wynik = 1;
+        if (n==1){
+            return 1;
+        }else{
+            return n*factorial(n-1);
+        }
     }
+
+    public boolean scoresIncreasing(int[] scores) {
+        boolean is_increasing = true;
+        for (int i = 0; i < (scores.length-1); i++){
+            if (scores[i+1] < scores[i]){
+                is_increasing = false;
+                break;
+            }
+        }return is_increasing;
+    }
+
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if (map.containsKey("a")){
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }return map;
+    }
+
+    public int luckySum(int a, int b, int c) {
+        if(a!=13){
+            if(b!=13){
+                if (c!=13){
+                    return a+b+c;
+                }else{
+                    return a+b;
+                }
+            }else{
+                return a;
+            }
+        }else{
+            return 0;
+        }
+    }
+
+
 
 
     public static void main(String[] args) {
--- Lab3/test/CodingBatTest.java
+++ Lab3/test/CodingBatTest.java
@@ -1,4 +1,48 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class CodingBatTest {
-  
+    CodingBat cb = new CodingBat();
+    @Test
+    public void close10() {
+        assertEquals(8, cb.close10(8,13));
+        assertEquals(8, cb.close10(13,8));
+        assertEquals(0, cb.close10(13,7));
+        assertEquals(0, cb.close10(7,13));
+        assertEquals(9, cb.close10(9,13));
+        assertEquals(0, cb.close10(0,20));
+    }
+
+    @Test
+    public void factorial() {
+        assertEquals(6, cb.factorial(3));
+        assertEquals(2, cb.factorial(2));
+        assertEquals(6, cb.factorial(3));
+        assertEquals(24, cb.factorial(4));
+        assertEquals(120, cb.factorial(5));
+        assertEquals(479001600, cb.factorial(12));
+
+
+    }
+
+    @Test
+    public void scoresIncreasing() {
+        assertEquals(true, cb.scoresIncreasing(new int[]{1,3,2}));
+        assertEquals(false, cb.scoresIncreasing(new int[]{1,3,2}));
+        assertEquals(true, cb.scoresIncreasing(new int[]{1,1,4}));
+        assertEquals(true, cb.scoresIncreasing(new int[]{1,1,2,4,3,7}));
+        assertEquals(true, cb.scoresIncreasing(new int[]{1,1,2,4,3,7}));
+        assertEquals(true, cb.scoresIncreasing(new int[]{-5,4,11}));
+
+    }
+
+    @Test
+    public void luckySum() {
+        assertEquals(6, cb.luckySum(1,2,3));
+        assertEquals(3, cb.luckySum(1,2,13));
+        assertEquals(14, cb.luckySum(13,12,3));
+        assertEquals(8, cb.luckySum(8,13,2));
+
+    }
 }
